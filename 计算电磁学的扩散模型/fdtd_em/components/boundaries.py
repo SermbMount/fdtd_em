f2fa808f3fca2b3ce67d899e76814a61e1c24803
sermb kakairrrrrrrrr <147632846+SermbMount@users.noreply.github.com>
2026-04-26 14:40:09 +0800

    Update boundaries.py

--- 计算电磁学的扩散模型/fdtd_em/components/boundaries.py
+++ 计算电磁学的扩散模型/fdtd_em/components/boundaries.py
@@ -1,11 +1,8 @@
 """
 fdtd_em.components.boundaries
-
 边界条件组件（Boundary Components）
-
 当前实现：
-- PMLBoundary：完美匹配层（Perfectly Matched Layer）
-
+PMLBoundary：匹配层（Perfectly Matched Layer）
 适配 fdtd==0.3.x
 """
 
@@ -24,7 +21,7 @@ class PMLBoundary:
         PML 厚度（网格点数）
         一般建议：10~20
     """
-    #默认值是保底机制，若在外部完全控制了真实参数，它根本不会生效。
+    #默认值是光吸收保底机制，若在外部完全控制了真实参数，报
     thickness: int = 10
 
     def apply(self, grid: fdtd.Grid) -> None:
@@ -34,9 +31,7 @@ class PMLBoundary:
         fdtd 的写法标准是：（该版本算法0.3.X）
         grid[0:thickness, :, :] = fdtd.PML(...)
         """
-
         t = self.thickness
-
         # x 方向
         grid[0:t, :, :] = fdtd.PML(name="pml_x_low")
         grid[-t:, :, :] = fdtd.PML(name="pml_x_high")
