4805a4a574ea90381fbd43ab6681be4e1575e44d
sermb kakairrrrrrrrr <147632846+SermbMount@users.noreply.github.com>
2026-04-26 14:38:11 +0800

    Update generation_config.py

--- 计算电磁学的扩散模型/fdtd_em/config/generation_config.py
+++ 计算电磁学的扩散模型/fdtd_em/config/generation_config.py
@@ -8,7 +8,7 @@
 #
 # 2. U-Net 深度学习显存占用 (与 Batch_Size 成正比，与网格呈平方关系)
 #    VRAM_UNet ≈ 1.5GB + Batch_Size * (N / 120)^2 * 0.3GB
-#    -> 当 N=120, Batch=16 时: 1.5 + 16 * 1 * 0.3 = 6.3GB (完美吃满 7G 可用显存，效率最高)
+#    -> 当 N=120, Batch=16 时: 1.5 + 16 * 1 * 0.3 = 6.3GB (吃满 7G 可用显存)
 #    -> 当 N=150, Batch=16 时: 1.5 + 16 * 1.56 * 0.3 ≈ 9.0GB (必定 OOM 报错显存溢出)
 =================================================================================
 """
@@ -37,7 +37,7 @@ freq_center = 3e14  # 光学频段用 3e14，如果是微波/雷达频段可以�
 # 空间步长 (单位: 米)。即每个网格代表 10nm 的实际物理尺寸。
 dt_factor = 0.99
 # 库朗数 (Courant factor) 缩放系数。取 0.99 是为了在满足 CFL 稳定性条件的前提下追求最大时间步，防止电磁波发散。
-steps = 500
+steps = 100
 # FDTD仿真的总时间步数。
 pml_thickness = 10
 # 层 (PML) 的厚度（网格数）。吸收到达边界的电磁波，模拟无限大自由空间，防止波反射回来干扰视线。
@@ -109,17 +109,18 @@ sphere_radius_max = 15.0    # 随机生成的最大半径
 # ===============================================================================
 # 5.数据生成与执行设置 (Pipeline Execution Settings)
 # ===============================================================================
-num_samples = 500  # 每次运行 generator.py 生成的样本总数。深度学习中，数据量越大，最终 MAE 误差越低。
+num_samples = 300  # 每次运行 generator.py 生成的样本总数。深度学习中，数据量越大，最终 MAE 误差越低。
 execution_mode = "local"  # 运行模式标记（后续若上超算集群可改为 "server"）。
 device = "cuda"  # 指定FDTD物理求解器的加速硬件。
 
 # ===============================================================================
 # 6.U-Net 扩散模型训练超参数 (Deep Learning Hyperparameters)
 # ===============================================================================
-batch_size = 8  # 每次喂给U-Net的图像数量。若报OOM（Out of Memory），请果断更改为当前使用的最大显存。
-epochs = 500  # 整个样本的数据集将被学习X遍。
+batch_size = 4  # 每次喂给U-Net的图像数量。若报OOM（Out of Memory），请果断更改为当前使用的最大显存。
+epochs = 30  # 整个样本的数据集将被学习X遍。
 learning_rate = 1e-4  # 学习率。决定了模型修改自己参数的步子大小。
 diffusion_steps = 1000  # DDPM 扩散与去噪的总步数。步数越多，生成的介电常数图像细节越细腻、边缘越锐利。
+
 
 def build_custom_structure(is_batch_generation=False) -> Region:
     if not is_batch_generation:
@@ -130,9 +131,12 @@ def build_custom_structure(is_batch_generation=False) -> Region:
         small_sphere = SphereRegion(center=(52, 52, 58), radius=7)
         my_structure = SubtractRegion(big_sphere, small_sphere)
         '''
+       #my_structure = AnnularRegion(center=(60, 60, 60), r_inner=6, r_outer=12)
+    #return my_structure
+        cx, cy, cz = 60, 60, 60
+        r = 10
+        return SphereRegion(center=(cx, cy, cz), radius=r)
 
-        my_structure = AnnularRegion(center=(60, 60, 60), r_inner=6, r_outer=12)
-        return my_structure
 
     else:
 
